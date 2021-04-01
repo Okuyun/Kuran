104fa9091b0f185af5e2616676d4283a671e15cb
M Akif Eyler <ma.eyler@gmail.com>
2021-04-01 17:05:32 +0300

    add Back button
    two buttons per page

--- ders/init.js
+++ ders/init.js
@@ -1,9 +1,14 @@
+function goBack() {
+    location.pathname = "/Kuran/ders/"
+}
 function init() {
     for (let a of document.querySelectorAll('a')) {
       if (!a.name) continue
       a.href = "/Kuran/reader.html#"+a.name
       a.target = "iqra"
     }
+    for (let b of document.querySelectorAll('.goBack'))
+      b.onclick = goBack
 }
 init()
   
